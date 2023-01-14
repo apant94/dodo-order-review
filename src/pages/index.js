066c990379ecdcfbf05f315838b9ab00a6927572
Anna Panteleeva <apanteleeva94@gmail.com>
2023-01-14 12:26:00 +0300

    feat: add :focus to inputs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+
+// Растягивание по мере заполнения отзыва
+
+const textarea = document.querySelector('.form__comment');
+
+// textarea.addEventListener('keyup', function(){
+//   console.log(this.style);
+//   if(this.scrollTop > 0){
+//     this.style.height = this.scrollHeight + "px";
+//   }
+// });
+
+function putComment() {
+  textarea.setAttribute("style", "height:" + (textarea.scrollHeight - 30) + "px;");
+  textarea.addEventListener("input", onInput, false);
+};
+
+function onInput() {
+  this.style.height = '28px';
+  this.style.height = (this.scrollHeight) + "px";
+};
+
+putComment();
