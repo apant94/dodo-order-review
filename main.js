f1e86000bacb2d06077a8d65a2267937ace3b3d9
Anna Panteleeva <apanteleeva94@gmail.com>
2023-01-14 13:25:00 +0300

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";var t=document.querySelector(".form__comment");t.setAttribute("style","height:"+(t.scrollHeight-30)+"px;"),t.addEventListener("input",(function(){this.style.height="28px",this.style.height=this.scrollHeight+"px"}),!1)})();
+(()=>{"use strict";var t=document.querySelector(".form__comment");t.setAttribute("style","height:"+(t.scrollHeight-38)+"px;"),t.addEventListener("input",(function(){this.style.height="54px",this.style.height=this.scrollHeight-38+"px"}),!1)})();
